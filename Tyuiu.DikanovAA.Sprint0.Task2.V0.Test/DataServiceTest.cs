9fb9b9038d424e52627bcffd56c43187bcd0f21c
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-09-20 19:16:32 +0500

    Task2

---
+++ Tyuiu.DikanovAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.DikanovAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.DikanovAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Артём";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Артём", res);
+        }
+    }
+}
